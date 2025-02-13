460c91688626fcd23482b79e7639220cee23e31b
Ilia Prokofev <beetlewar010785@gmail.com>
2025-02-13 15:08:30 +0100

    try increase index size

--- src/SortTask.Adapter/AdapterConst.cs
+++ src/SortTask.Adapter/AdapterConst.cs
@@ -4,7 +4,7 @@ namespace SortTask.Adapter;
 
 public static class AdapterConst
 {
-    public const int NumIndexOphWords = 4;
+    public const int NumIndexOphWords = 8;
     public const int BTreeOrder = 10;
     public static readonly Encoding Encoding = Encoding.UTF8;
 }
